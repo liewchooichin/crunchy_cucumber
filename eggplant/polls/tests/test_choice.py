62401833f0d715d45019eb1d606c504b1d30ebea
liewchooichin <liewchooichin@gmail.com>
2024-07-25 15:20:26 +0800

    test for choice, recent list

--- eggplant/polls/tests/test_choice.py
+++ eggplant/polls/tests/test_choice.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from datetime import datetime
 
@@ -21,7 +22,11 @@ class TestChoice(TestCase):
             choice_text = "1234567890 1234567890 1234567890 1234567890",
             question = self.test_question
         )
-
+        self.test_choice = Choice.objects.create(
+            id=3004, # an unlikely id
+            choice_text = "1234567890 1234567890 1234567890 1234567890",
+            question = self.test_question
+        )
     def test_choice_data(self):
         """Test that the test data is what was created
             in the database.
@@ -55,4 +60,56 @@ class TestChoice(TestCase):
         """
         choice_str = "Choice 3003, 1234567890 1234567890 12345678"
         self.assertEqual(first=choice_str, second=self.test_choice.__str__())
+
+    def test_empty_choices(self):
+        """Test for empty choices. The page should show a message
+            like "no choices".
+        """
+        question = Question.objects.create(
+                        question_text = "test question 1",
+                        pub_date = datetime(year=2024, month=1, day=1),
+                    )
+        # The question does not have any choices.
+        # When the question_detail is shown, there choices will be empty.
+        page_url = reverse("polls:question_detail", 
+                           kwargs={"question_id": question.id})
+        response = self.client.get(path=page_url)
+        self.assertEqual(first=200, second=response.status_code)
+        question_from_response = response.context["question"]
+        # validate the question is the test question
+        self.assertEqual(question, question_from_response)
+        # validate that the query set for choice is empty
+        self.assertQuerySetEqual(question_from_response.choice_set.all(), [])
+        
+    def test_choice_view(self):
+        """Test for empty choices. The page should show a message
+            like "no choices".
+        """
+        question = Question.objects.create(
+                        question_text = "test question 1",
+                        pub_date = datetime(year=2024, month=1, day=1),
+                    )
+        choice_1 = Choice.objects.create(
+            choice_text = "test choice 1",
+            votes = 0,
+            question = question
+        )
+        choice_2 = Choice.objects.create(
+            choice_text = "test choice 2",
+            votes = 0,
+            question = question
+        )
+        page_url = reverse("polls:question_detail", 
+                           kwargs={"question_id": question.id})
+        response = self.client.get(path=page_url)
+        self.assertEqual(first=200, second=response.status_code)
+        question_from_response = response.context["question"]
+        # validate the question is the test question
+        self.assertEqual(question, question_from_response)
+        # validate that the query set for choice is empty
+        choice_from_response_1 = response.context["question"].choice_set.get(id=choice_1.id)
+        self.assertEqual(choice_from_response_1.choice_text, choice_1.choice_text)
+        choice_from_response_2 = response.context["question"].choice_set.get(id=choice_2.id)
+        self.assertEqual(choice_from_response_2.choice_text, choice_2.choice_text)
+        
 
